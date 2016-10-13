ead46d087ddaf7a8a343542a7b9ba52b76ad327b
saniyamazman <mazmans@uw.edu>
2016-10-13 10:23:59 -0700

    Participation points

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -6,27 +6,29 @@ var changeText;
 // You'll have to wait for you page to load to assign events to the elements created in your index.html File
 $(function(){
   // Select elements with class "italics" and set their css "font-style", to "italic"
-
+$('.italics').css('font-style', 'italic');
 
   // Select all svgs and change their attr "height" to 200
-
+$('svg').attr('height', '200');
 
   // Select your first svg and change it's width attribute to 10
-
+$("svg:first").attr('width', '10');
 
   // Select even rows in the table and set their css "background" to "lightgray"
-
+$("tr:even").css('background', 'lightgray');
 
   // Select the input and set the attribute "placeholder" to "start typing"
-
+$("input").attr('placeholder', 'start typing');
 
   // Set the input val to "hello"
-
+$('input').val('');
 
 
   // Fill in this function that selects the text with id "change" and sets it text to the value in the text box.  Click "submit" to see if it worked.
+  //changes "This will be fun " to whatever that you set the value equal to
   changeText = function() {
-
+  	var val = $('input').val();
+	$('#change').text(val);
 
   }
 });
--- exercise-3/js/main.js
+++ exercise-3/js/main.js
@@ -3,9 +3,30 @@
 // You'll have to wait for you page to load to assign events to the elements created in your index.html File
 $(function() {
   // Select all <p> elements and assign a click event that removes the clicked element (recall `this`)
+  $("p").on('click', function(){
+  	$(this).remove();
+})
   // Select the <circle>, and when it's clicked, turn it red
+  $("circle").on('click', function(){
+  	$(this).css('fill', 'red');
+  })
   // Each time the <circle> is clicked, change its radius from 10 to 40
-  // Assign a mouseenter event to the <rect> element that makes it have an opacity of .5
-  // Assign a mouseleave event to the <rect> element that makes it have an opacity of 1
+  if ($("circle").on('click', function() {
+  	var currR = $(this).attr('r');
+  	if (currR == 10) {
+  		$(this).attr('r', '10');
+  	} else {
+  		$(this).attr('r', '40');
+  	}
+  	}  
+  })
 
+  // Assign a mouseenter event to the <rect> element that makes it have an opacity of .5
+  $("rect").mouseenter(function(){
+  	$(this).css('opacity', '.5');
+  })
+  // Assign a mouseleave event to the <rect> element that makes it have an opacity of 1
+  $("rect").mouseleave(function(){
+  	$(this).css('opacity', '1');
+  })
 });
